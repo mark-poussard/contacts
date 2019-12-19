493cda011453e4de3c82633ce88cf9f835fa7b46
Mark <poussard.mark@gmail.com>
2019-12-20 00:22:06 +0100

    Updates

---
+++ precache-manifest.4af0e3308fad757f90945e71b7771bd1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "05afaf23b784b796c5b7",
+    "url": "/contacts/static/js/runtime~main.05afaf23.js"
+  },
+  {
+    "revision": "fd22ccd7eb8b692d7d64",
+    "url": "/contacts/static/js/main.fd22ccd7.chunk.js"
+  },
+  {
+    "revision": "beb3d5e24892742e3cb6",
+    "url": "/contacts/static/js/1.beb3d5e2.chunk.js"
+  },
+  {
+    "revision": "fd22ccd7eb8b692d7d64",
+    "url": "/contacts/static/css/main.4af85c57.chunk.css"
+  },
+  {
+    "revision": "db9b5be04bcbbbf12eb72860401588f0",
+    "url": "/contacts/index.html"
+  }
+];
