637d66573f86b561d153df95e660fa475739e9a3
Mark <poussard.mark@gmail.com>
2019-12-20 17:54:42 +0100

    Added csv data source support

--- src/business/network/DataStore.ts
+++ src/business/network/DataStore.ts
@@ -1,17 +1,34 @@
 import Contact from "../../model/contacts/Contact";
+import CsvDeserializer from "../deserialization/CsvDeserializer";
 
-const DATA_STORE_ENDPOINT = "https://gist.githubusercontent.com/mark-poussard/d9f54694ae06cf92c8528725d30f7c2c/raw/contacts.json";
+const DATA_STORE_ENDPOINT_JSON = "https://gist.githubusercontent.com/mark-poussard/d9f54694ae06cf92c8528725d30f7c2c/raw/contacts.json";
+const DATA_STORE_ENDPOINT_CSV = "https://gist.githubusercontent.com/mark-poussard/5255c223752c695ba0846971cbd8d039/raw/contacts.csv";
 
 class DataStore{
     
-    getContacts = () => {
-        return fetch(DATA_STORE_ENDPOINT)
+    private getContactsFromJsonSource = () => {
+        return fetch(DATA_STORE_ENDPOINT_JSON)
             .then(response => {
                 if(response.ok){
                     return response.json();
                 }
                 throw new Error("Couldn't reach data store.");
+            });
+    }
+
+    private getContactsFromCsvSource = () => {
+        return fetch(DATA_STORE_ENDPOINT_CSV)
+            .then(response => {
+                if(response.ok){
+                    return response.text();
+                }
+                throw new Error("Couldn't reach data store.");
             })
+            .then(CsvDeserializer.deserialize);
+    }
+
+    getContacts = () => {
+        return this.getContactsFromCsvSource()
             .then((dto : any[]) => dto.map((x:any, idx : number) => Contact.fromDto(x, idx)));
     }
 
